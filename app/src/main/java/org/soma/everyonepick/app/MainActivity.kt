f7ec1358a9e60a4f48ff2a3fc3f57f5afe9b3709
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-01 17:06:38 +0900

    Implement Bottom Navagation using Jetpack Navagation

--- app/src/main/java/org/soma/everyonepick/app/MainActivity.kt
+++ app/src/main/java/org/soma/everyonepick/app/MainActivity.kt
@@ -2,10 +2,22 @@ package org.soma.everyonepick.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import org.soma.everyonepick.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentcontainerview_navhost) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        binding.bottomnavigationview.setupWithNavController(navController)
     }
 }
