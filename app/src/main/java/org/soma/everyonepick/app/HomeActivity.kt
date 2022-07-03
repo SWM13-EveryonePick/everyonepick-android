9988f809a840f146d3e5f957f63570cd06f90bc2
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-03 11:25:34 +0900

    [PICK-13] FEAT: 카메라 프래그먼트 전환 시 풀스크린 모드

--- app/src/main/java/org/soma/everyonepick/app/HomeActivity.kt
+++ app/src/main/java/org/soma/everyonepick/app/HomeActivity.kt
@@ -1,7 +1,9 @@
 package org.soma.everyonepick.app
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -15,8 +17,53 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
 
+        initializeNavigation()
+    }
+
+    private fun initializeNavigation() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
         val navController = navHostFragment.findNavController()
-        binding.bottomnavigationview.setupWithNavController(navController)
+        binding.bottomnavigationview.run{
+            setupWithNavController(navController)
+            setOnItemSelectedListener { item ->
+                // Camera Fragment에서 풀스크린을 사용합니다.
+                setFullScreenMode(item.itemId == R.id.cameraFragment)
+                return@setOnItemSelectedListener true
+            }
+        }
+    }
+
+    private fun setFullScreenMode(flag: Boolean) {
+        if(flag) {
+            hideStatusBar()
+            supportActionBar?.hide()
+            binding.bottomnavigationview.visibility = View.GONE
+        }else{
+            showStatusBar()
+            supportActionBar?.show()
+            binding.bottomnavigationview.visibility = View.VISIBLE
+        }
+    }
+
+    private fun hideStatusBar() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }else{
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+    }
+
+    private fun showStatusBar() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.setDecorFitsSystemWindows(true)
+            window.insetsController?.show(WindowInsets.Type.statusBars())
+        }else{
+            window.addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        }
     }
 }
