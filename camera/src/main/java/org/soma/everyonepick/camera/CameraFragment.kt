264c52dbc3a64b6c5ad4939eebef794766d510c5
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-03 19:18:17 +0900

    [PICK-13] FEAT: Camera Preview View 구현

--- camera/src/main/java/org/soma/everyonepick/camera/CameraFragment.kt
+++ camera/src/main/java/org/soma/everyonepick/camera/CameraFragment.kt
@@ -5,6 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.common.util.concurrent.ListenableFuture
 import org.soma.everyonepick.camera.databinding.FragmentCameraBinding
 
 class CameraFragment(
@@ -13,6 +19,27 @@ class CameraFragment(
 ) : Fragment() {
     private var _binding: FragmentCameraBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
+        val preview = Preview.Builder().build()
+        val cameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+
+        preview.setSurfaceProvider(binding.previewview.surfaceProvider)
+        cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
