639bacff2f27a86993258f1b546ef7b93ea2d8fb
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-04 16:17:33 +0900

    [PICK-13] FEAT: Preview Fragment 분리

--- camera/src/main/java/org/soma/everyonepick/camera/CameraFragment.kt
+++ camera/src/main/java/org/soma/everyonepick/camera/CameraFragment.kt
@@ -10,6 +10,7 @@ import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
+import androidx.navigation.Navigation
 import com.google.common.util.concurrent.ListenableFuture
 import org.soma.everyonepick.camera.databinding.FragmentCameraBinding
 
@@ -20,36 +21,11 @@ class CameraFragment(
     private var _binding: FragmentCameraBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
-        cameraProviderFuture.addListener({
-            val cameraProvider = cameraProviderFuture.get()
-            bindPreview(cameraProvider)
-        }, ContextCompat.getMainExecutor(requireContext()))
-    }
-
-    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
-        val preview = Preview.Builder().build()
-        val cameraSelector = CameraSelector.Builder()
-            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-            .build()
-
-        preview.setSurfaceProvider(binding.previewview.surfaceProvider)
-        cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentCameraBinding.inflate(inflater, container, false)
-
-        binding.lifecycleOwner = this
-        binding.fragment = this
-
         return binding.root
     }
 
