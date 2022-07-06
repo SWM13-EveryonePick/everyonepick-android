809a34563c0a3d213bf3ad1ef3818557770bd1b7
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-06 13:28:50 +0900

    [PICK-14] FEAT: FROnnxMobileNet 구현

--- camera/src/main/java/org/soma/everyonepick/camera/fragments/PreviewFragment.kt
+++ camera/src/main/java/org/soma/everyonepick/camera/fragments/PreviewFragment.kt
@@ -1,19 +1,27 @@
 package org.soma.everyonepick.camera.fragments
 
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.Preview
+import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
-import com.google.common.util.concurrent.ListenableFuture
-import org.soma.everyonepick.camera.R
 import org.soma.everyonepick.camera.databinding.CameraUiContainerBinding
 import org.soma.everyonepick.camera.databinding.FragmentPreviewBinding
+import org.soma.everyonepick.camera.utility.FROnnxMobileNet
+import java.nio.ByteBuffer
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+
 
 class PreviewFragment : Fragment() {
     private var _binding: FragmentPreviewBinding? = null
@@ -21,7 +29,13 @@ class PreviewFragment : Fragment() {
 
     private var cameraUiContainerBinding: CameraUiContainerBinding? = null
 
-    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+    private var processCameraProvider: ProcessCameraProvider? = null
+    private var preview: Preview? = null
+    private var imageCapture: ImageCapture? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var camera: Camera? = null
+
+    private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,26 +48,12 @@ class PreviewFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        updateCameraUi()
-        setUpCamera()
-    }
+        cameraExecutor = Executors.newSingleThreadExecutor()
 
-    private fun setUpCamera() {
-        cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
-        cameraProviderFuture.addListener({
-            val cameraProvider = cameraProviderFuture.get()
-            bindPreview(cameraProvider)
-        }, ContextCompat.getMainExecutor(requireContext()))
-    }
-
-    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
-        val preview = Preview.Builder().build()
-        val cameraSelector = CameraSelector.Builder()
-            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-            .build()
-
-        preview.setSurfaceProvider(binding.previewview.surfaceProvider)
-        cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+        binding.previewview.post {
+            updateCameraUi()
+            setUpCamera()
+        }
     }
 
     private fun updateCameraUi() {
@@ -66,11 +66,95 @@ class PreviewFragment : Fragment() {
             binding.constraintlayoutRoot,
             true
         )
+
+        cameraUiContainerBinding?.imagebuttonShutter?.setOnClickListener {
+            imageCapture?.let { imageCapture ->
+                // TODO: imageCapture.takePicture
+            }
+        }
+    }
+
+    private fun setUpCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener({
+            processCameraProvider = cameraProviderFuture.get()
+            bindCameraUseCases()
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun bindCameraUseCases() {
+        val screenAspectRatio = calculateAspectRatio()
+        val cameraProvider = processCameraProvider
+            ?: throw IllegalStateException("Camera initialization failed.")
+        // TODO: 화면 전환
+        val cameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+
+        // Preview
+        preview = Preview.Builder()
+            .setTargetAspectRatio(screenAspectRatio)
+            .build()
+
+        // ImageCapture
+        imageCapture = ImageCapture.Builder()
+            .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+            .setTargetAspectRatio(screenAspectRatio)
+            .setJpegQuality(50)
+            .build()
+
+        // ImageAnalyzer
+        imageAnalyzer = ImageAnalysis.Builder()
+            .setTargetAspectRatio(screenAspectRatio)
+            .build()
+            .also {
+                it.setAnalyzer(cameraExecutor, FROnnxMobileNet(requireContext()) { floatArray ->
+                    Log.d(TAG, floatArray.contentToString())
+                })
+            }
+
+        cameraProvider.unbindAll()
+
+        try {
+            preview?.setSurfaceProvider(binding.previewview.surfaceProvider)
+            camera = cameraProvider.bindToLifecycle(
+                this as LifecycleOwner, cameraSelector, preview, imageCapture, imageAnalyzer
+            )
+        } catch (e: Exception) {
+            Log.e(TAG, "Use case binding failed", e)
+        }
+    }
+
+    private fun calculateAspectRatio(): Int {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val metrics = requireActivity().windowManager.currentWindowMetrics.bounds
+            aspectRatio(metrics.width(), metrics.height())
+        } else {
+            val metrics = resources.displayMetrics
+            aspectRatio(metrics.widthPixels, metrics.heightPixels)
+        }
+    }
+
+    private fun aspectRatio(width: Int, height: Int): Int {
+        val previewRatio = max(width, height).toDouble() / min(width, height)
+        return if(abs(previewRatio - RATIO_4_3_VALUE) <= abs(previewRatio - RATIO_16_9_VALUE)) {
+            AspectRatio.RATIO_4_3
+        } else {
+            AspectRatio.RATIO_16_9
+        }
     }
 
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "PreviewFragment"
+        private const val RATIO_4_3_VALUE = 4.0 / 3.0
+        private const val RATIO_16_9_VALUE = 16.0 / 9.0
     }
 }
