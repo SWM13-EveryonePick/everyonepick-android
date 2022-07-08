ad188b522117c1364a7d73c9910173afeff38387
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-09 00:30:48 +0900

    [PICK-32] FEAT: 권한을 거부했을 때 표시되는,  설정 어플을 실행하는 버튼  구현

--- login/src/main/java/org/soma/everyonepick/login/ui/faceinformation/camerafragments/PermissionFragment.kt
+++ login/src/main/java/org/soma/everyonepick/login/ui/faceinformation/camerafragments/PermissionFragment.kt
@@ -1,17 +1,27 @@
 package org.soma.everyonepick.login.ui.faceinformation.camerafragments
 
 import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import org.soma.everyonepick.common.Permission.Companion.hasAllPermissions
+import org.soma.everyonepick.login.databinding.FragmentPermissionBinding
 
 private val PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.CAMERA)
 
 class PermissionFragment : Fragment() {
+    private var _binding: FragmentPermissionBinding? = null
+    private val binding get() = _binding!!
+
     private val requestPermissionsLauncher = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
     ) { isGranted ->
@@ -22,8 +32,20 @@ class PermissionFragment : Fragment() {
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPermissionBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.fragment = this
+
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
 
         if (!hasAllPermissions(requireContext(), PERMISSIONS_REQUIRED)) {
             requestPermissionsLauncher.launch(PERMISSIONS_REQUIRED)
@@ -38,5 +60,14 @@ class PermissionFragment : Fragment() {
                 PermissionFragmentDirections.actionPermissionToPreview()
             )
         }
+    }
+
+
+    fun onClickStartSettingForPermissionButton() {
+        val intent = Intent(
+            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+            Uri.parse("package:${activity?.packageName}")
+        )
+        startActivity(intent)
     }
 }
