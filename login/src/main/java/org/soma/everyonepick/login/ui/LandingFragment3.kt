fc1b01cf0715cab7e5aa1567ebabe3dca7a423f7
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-09 21:34:35 +0900

    [PICK-16] ADD: LandingFragment 3개 추가

---
+++ login/src/main/java/org/soma/everyonepick/login/ui/LandingFragment3.kt
@@ -0,0 +1,30 @@
+package org.soma.everyonepick.login.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.soma.everyonepick.login.R
+import org.soma.everyonepick.login.databinding.FragmentLanding3Binding
+
+class LandingFragment3 : Fragment() {
+    private var _binding: FragmentLanding3Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLanding3Binding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.fragment = this
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
