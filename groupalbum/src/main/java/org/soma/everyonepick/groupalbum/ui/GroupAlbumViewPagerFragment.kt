19f805328fa3379e5e38def82c4bd3c6a584fdc1
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-11 17:42:58 +0900

    [PICK-37] ADD: TabLayout

--- groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/GroupAlbumViewPagerFragment.kt
+++ groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/GroupAlbumViewPagerFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.tabs.TabLayoutMediator
 import org.soma.everyonepick.groupalbum.R
 import org.soma.everyonepick.groupalbum.adapter.GroupAlbumAdapter
 import org.soma.everyonepick.groupalbum.databinding.FragmentGroupalbumviewpagerBinding
+
+private val TAB_ITEMS = listOf("앨범", "친구 목록")
 
 class GroupAlbumViewPagerFragment : Fragment() {
     private var _binding: FragmentGroupalbumviewpagerBinding? = null
@@ -23,7 +26,11 @@ class GroupAlbumViewPagerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         binding.viewpager2.adapter = GroupAlbumAdapter(this)
+        TabLayoutMediator(binding.tablayout, binding.viewpager2) { tab, position ->
+            tab.text = TAB_ITEMS[position]
+        }.attach()
     }
 
     override fun onDestroy() {
