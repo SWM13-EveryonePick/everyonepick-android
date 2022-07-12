5f258997c48973205650d96cf66ec8c665664643
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-12 17:00:47 +0900

    [PICK-37] FEAT: 카카오톡 친구 목록 가져오기

--- groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/friend/FriendFragment.kt
+++ groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/friend/FriendFragment.kt
@@ -1,18 +1,45 @@
 package org.soma.everyonepick.groupalbum.ui.friend
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.kakao.sdk.talk.TalkApiClient
 import org.soma.everyonepick.groupalbum.R
+import org.soma.everyonepick.groupalbum.databinding.FragmentFriendBinding
 
 class FriendFragment : Fragment() {
+    private var _binding: FragmentFriendBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        TalkApiClient.instance.friends { friends, error ->
+            if (error != null) {
+                Log.e(TAG, "카카오톡 친구 목록 가져오기 실패", error)
+            }else if (friends != null){
+                Log.i(TAG, "카카오톡 친구 목록 가져오기 성공")
+            }
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friend, container, false)
+        _binding = FragmentFriendBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    companion object {
+        private const val TAG = "FriendFragment"
     }
 }
