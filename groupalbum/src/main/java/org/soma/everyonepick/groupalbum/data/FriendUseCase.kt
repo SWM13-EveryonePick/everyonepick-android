1a8424cca4bbbe0592bf7fb9ffeffa9a7857e8a6
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-13 14:25:32 +0900

    [PICK-37] FEAT: 카카오 친구목록 API로 뷰모델 구현 및 기존 Friend Repository, UseCase를 제거

--- groupalbum/src/main/java/org/soma/everyonepick/groupalbum/data/FriendUseCase.kt
+++
@@ -1,9 +0,0 @@
-package org.soma.everyonepick.groupalbum.data
-
-import javax.inject.Inject
-
-class FriendUseCase @Inject constructor(
-    private val friendRepository: FriendRepository
-) {
-    fun getFriends() = friendRepository.getFriends()
-}
--- groupalbum/src/main/java/org/soma/everyonepick/groupalbum/viewmodel/FriendViewModel.kt
+++ groupalbum/src/main/java/org/soma/everyonepick/groupalbum/viewmodel/FriendViewModel.kt
@@ -1,26 +1,30 @@
 package org.soma.everyonepick.groupalbum.viewmodel
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.kakao.sdk.talk.TalkApiClient
 import com.kakao.sdk.talk.model.Friend
 import com.kakao.sdk.talk.model.Friends
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import org.soma.everyonepick.groupalbum.data.FriendUseCase
 import javax.inject.Inject
 
 @HiltViewModel
-class FriendViewModel @Inject internal constructor(
-    private val friendUseCase: FriendUseCase
-): ViewModel() {
+class FriendViewModel: ViewModel() {
     val friends: MutableLiveData<Friends<Friend>> = MutableLiveData()
     init {
         updateFriends()
     }
 
     fun updateFriends() {
-        GlobalScope.launch {
-            friends.postValue(friendUseCase.getFriends())
+        TalkApiClient.instance.friends { newFriends, error ->
+            if(error != null) {
+                Log.e("TAG", "카카오톡 친구 목록 가져오기 실패", error)
+            } else if(newFriends != null) {
+                Log.i("TAG", "카카오톡 친구 목록 가져오기 성공")
+                friends.postValue(newFriends)
+            }
         }
     }
 }
