57df559fee1087a318b16bdab096c58b5be8b4ff
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-14 12:38:18 +0900

    [PICK-49] ADD: group album recycler view

--- groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/groupalbum/GroupAlbumFragment.kt
+++ groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/groupalbum/GroupAlbumFragment.kt
@@ -6,12 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import org.soma.everyonepick.groupalbum.R
+import org.soma.everyonepick.groupalbum.adapter.GroupAlbumAdapter
+import org.soma.everyonepick.groupalbum.databinding.FragmentGroupalbumBinding
 
 class GroupAlbumFragment : Fragment() {
+    private var _binding: FragmentGroupalbumBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_groupalbum, container, false)
+        _binding = FragmentGroupalbumBinding.inflate(inflater, container, false)
+        initializeRecyclerView()
+
+        return binding.root
+    }
+
+    private fun initializeRecyclerView() {
+        val adapter = GroupAlbumAdapter()
+        binding.recyclerviewGroupalbum.adapter = adapter
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
