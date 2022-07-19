64fa9441700e02e7736076375ff609daa6234e0f
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-19 15:26:23 +0900

    [PICK-53] FEAT: GroupAlbumViewPagerAdapter, PhotoListFragment

--- groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/groupalbum/GroupAlbumViewPagerFragment.kt
+++ groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/groupalbum/GroupAlbumViewPagerFragment.kt
@@ -6,7 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
+import org.soma.everyonepick.groupalbum.adapter.GroupAlbumViewPagerAdapter
 import org.soma.everyonepick.groupalbum.databinding.FragmentGroupalbumviewpagerBinding
+
+private val TAB_ITEMS = listOf("사진", "합성중", "합성완료")
 
 class GroupAlbumViewPagerFragment : Fragment() {
     private var _binding: FragmentGroupalbumviewpagerBinding? = null
@@ -21,5 +26,16 @@ class GroupAlbumViewPagerFragment : Fragment() {
         _binding = FragmentGroupalbumviewpagerBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.viewpager2.let {
+            it.adapter = GroupAlbumViewPagerAdapter(this)
+        }
+        TabLayoutMediator(binding.tablayout, binding.viewpager2) { tab, position ->
+            tab.text = TAB_ITEMS[position]
+        }.attach()
     }
 }
---
+++ groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/groupalbum/PhotoListFragment.kt
@@ -0,0 +1,18 @@
+package org.soma.everyonepick.groupalbum.ui.groupalbum
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.soma.everyonepick.groupalbum.R
+
+class PhotoListFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_photo_list, container, false)
+    }
+}
