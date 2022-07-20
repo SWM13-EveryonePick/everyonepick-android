102d3fc5338c55b6213671fb97ddd9f01a85141c
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-20 10:52:27 +0900

    [PICK-69] ADD: GroupAlbumViewPagerViewModel 주석, REMOVE: 불필요한 코드

--- groupalbum/src/main/java/org/soma/everyonepick/groupalbum/adapter/GroupAlbumViewPagerAdapter.kt
+++ groupalbum/src/main/java/org/soma/everyonepick/groupalbum/adapter/GroupAlbumViewPagerAdapter.kt
@@ -4,10 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import org.soma.everyonepick.groupalbum.ui.groupalbum.PhotoListFragment
 
-class GroupAlbumViewPagerAdapter(
-    fragment: Fragment,
-    private val groupAlbumId: Long
-): FragmentStateAdapter(fragment) {
+class GroupAlbumViewPagerAdapter(fragment: Fragment): FragmentStateAdapter(fragment) {
     override fun getItemCount() = 3
 
     override fun createFragment(position: Int): Fragment {
