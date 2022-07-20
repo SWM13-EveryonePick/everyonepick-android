ff331faf45bff0199032edab6f3414f36b8dc2f6
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-20 16:07:32 +0900

    [PICK-69] TEST: PhotoListViewModel

--- groupalbum/src/androidTest/java/org/soma/everyonepick/groupalbum/utility/TestUtils.kt
+++ groupalbum/src/androidTest/java/org/soma/everyonepick/groupalbum/utility/TestUtils.kt
@@ -2,6 +2,11 @@ package org.soma.everyonepick.groupalbum.utility
 
 import org.soma.everyonepick.groupalbum.data.GroupAlbumDao
 import org.soma.everyonepick.groupalbum.data.GroupAlbumItem
+import org.soma.everyonepick.groupalbum.data.PhotoDao
+import org.soma.everyonepick.groupalbum.data.PhotoItem
 
 val testGroupAlbumDao = GroupAlbumDao(0, "test")
 val testGroupAlbumItem = GroupAlbumItem(testGroupAlbumDao, false, false)
+
+val testPhotoDao = PhotoDao(0, "test")
+val testPhotoItem = PhotoItem(testPhotoDao, false, false)
---
+++ groupalbum/src/androidTest/java/org/soma/everyonepick/groupalbum/viewmodel/PhotoListViewModelTest.kt
@@ -0,0 +1,45 @@
+package org.soma.everyonepick.groupalbum.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+import org.soma.everyonepick.groupalbum.data.PhotoRepository
+import org.soma.everyonepick.groupalbum.utility.testPhotoItem
+import javax.inject.Inject
+
+@HiltAndroidTest
+class PhotoListViewModelTest {
+    private lateinit var viewModel: PhotoListViewModel
+    private val hiltRule = HiltAndroidRule(this)
+    private val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val rule = RuleChain
+        .outerRule(hiltRule)
+        .around(instantTaskExecutorRule)
+
+    @Inject
+    lateinit var photoRepository: PhotoRepository
+
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+        viewModel = PhotoListViewModel(photoRepository)
+        viewModel.photoItemList.value = mutableListOf(testPhotoItem)
+    }
+
+    @Test
+    fun testSetIsCheckboxVisible() {
+        runBlocking {
+            viewModel.photoItemList.value!![0].isCheckboxVisible = false
+            viewModel.setIsCheckboxVisible(true)
+            assertTrue(viewModel.photoItemList.value!![0].isCheckboxVisible)
+        }
+    }
+}
