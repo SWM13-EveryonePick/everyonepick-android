ff70512f51892342981e7fe34cd1b70a6b1676ed
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-21 19:00:44 +0900

    [PICK-55] FEAT: 이미지 뷰 바인딩

--- common-ui/src/main/java/org/soma/everyonepick/common_ui/BindingAdapters.kt
+++ common-ui/src/main/java/org/soma/everyonepick/common_ui/BindingAdapters.kt
@@ -3,6 +3,7 @@ package org.soma.everyonepick.common_ui
 import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 import org.soma.everyonepick.common_ui.CustomActionBar
 
 object BindingAdapters {
@@ -10,5 +11,13 @@ object BindingAdapters {
     @BindingAdapter("onClickBackButton")
     fun bindBackButton(view: CustomActionBar, onClickBackButton: View.OnClickListener) {
         view.findViewById<ImageView>(R.id.image_backbutton).setOnClickListener(onClickBackButton)
+    }
+
+    @JvmStatic
+    @BindingAdapter("photoUrl")
+    fun bindImageView(imageView: ImageView, photoUrl: String) {
+        Glide.with(imageView.context)
+            .load(photoUrl)
+            .into(imageView)
     }
 }
--- groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/groupalbum/photo/PhotoFragment.kt
+++ groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/groupalbum/photo/PhotoFragment.kt
@@ -5,13 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import org.soma.everyonepick.groupalbum.R
 import org.soma.everyonepick.groupalbum.databinding.FragmentPhotoBinding
+import org.soma.everyonepick.groupalbum.viewmodel.PhotoViewModel
 
 class PhotoFragment : Fragment() {
     private var _binding: FragmentPhotoBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: PhotoViewModel by viewModels()
+    private val args: PhotoFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,8 +26,16 @@ class PhotoFragment : Fragment() {
         _binding = FragmentPhotoBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
         binding.fragment = this
+        binding.viewModel = viewModel
+
+        viewModel.photoUrl.value = args.photoUrl
 
         return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 
