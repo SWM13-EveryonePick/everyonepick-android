0c76c95def02be71fe0a88ec966f44fa077e5b70
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-23 01:27:22 +0900

    [PICK-54] FEAT: ImagePickerViewModel

--- groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/groupalbum/photo/ImagePickerFragment.kt
+++ groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/groupalbum/photo/ImagePickerFragment.kt
@@ -6,18 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import org.soma.everyonepick.common.HomeActivityUtility
 import org.soma.everyonepick.groupalbum.R
 import org.soma.everyonepick.groupalbum.adapter.ImageAdapter
 import org.soma.everyonepick.groupalbum.databinding.FragmentImagepickerBinding
-
-private val INDEX_MEDIA_ID = MediaStore.MediaColumns._ID
-private val INDEX_MEDIA_URI = MediaStore.MediaColumns.DATA
-private val INDEX_ALBUM_NAME = MediaStore.Images.Media.BUCKET_DISPLAY_NAME
-private val INDEX_DATE_ADDED = MediaStore.MediaColumns.DATE_ADDED
+import org.soma.everyonepick.groupalbum.viewmodel.ImagePickerViewModel
 
 class ImagePickerFragment : Fragment() {
     private var _binding: FragmentImagepickerBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: ImagePickerViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,40 +28,22 @@ class ImagePickerFragment : Fragment() {
         val adapter = ImageAdapter()
         binding.recyclerviewImage.adapter = adapter
 
+        viewModel.fetchImageItemList(requireContext())
         subscribeUi(adapter)
+
+        (activity as HomeActivityUtility).hideBottomNavigationView()
 
         return binding.root
     }
 
     private fun subscribeUi(adapter: ImageAdapter) {
-
+        viewModel.imageItemList.observe(viewLifecycleOwner) { imageItemList ->
+            adapter.submitList(imageItemList)
+        }
     }
 
-    /*@SuppressLint("Range")
-    private fun f() {
-        val uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-        val projection = arrayOf(
-            INDEX_MEDIA_ID,
-            INDEX_MEDIA_URI,
-            INDEX_ALBUM_NAME,
-            INDEX_DATE_ADDED
-        )
-        val selection =
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) MediaStore.Images.Media.SIZE + " > 0"
-            else null
-        val sortOrder = "$INDEX_DATE_ADDED DESC"
-        val cursor =
-            requireContext().contentResolver.query(uri, projection, selection, null, sortOrder)
-
-        val albumList = MutableLiveData<MutableList<Uri>>()
-        cursor?.let {
-            while(cursor.moveToNext()) {
-                val mediaPath = cursor.getString(cursor.getColumnIndex(INDEX_MEDIA_URI))
-                albumList.value?.add(Uri.fromFile(File(mediaPath)))
-                Log.e("ASD", Uri.fromFile(File(mediaPath)).toString())
-            }
-        }
-
-        cursor?.close()
-    }*/
+    override fun onDestroy() {
+        super.onDestroy()
+        (activity as HomeActivityUtility).showBottomNavigationView()
+    }
 }
---
+++ groupalbum/src/main/java/org/soma/everyonepick/groupalbum/viewmodel/ImagePickerViewModel.kt
@@ -0,0 +1,47 @@
+package org.soma.everyonepick.groupalbum.viewmodel
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import org.soma.everyonepick.groupalbum.data.ImageItem
+import java.io.File
+
+private val INDEX_MEDIA_ID = MediaStore.MediaColumns._ID
+private val INDEX_MEDIA_URI = MediaStore.MediaColumns.DATA
+private val INDEX_ALBUM_NAME = MediaStore.Images.Media.BUCKET_DISPLAY_NAME
+private val INDEX_DATE_ADDED = MediaStore.MediaColumns.DATE_ADDED
+
+class ImagePickerViewModel: ViewModel() {
+    val imageItemList = MutableLiveData<MutableList<ImageItem>>(mutableListOf())
+
+    @SuppressLint("Range")
+    fun fetchImageItemList(context: Context) {
+        val uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        val projection = arrayOf(
+            INDEX_MEDIA_ID,
+            INDEX_MEDIA_URI,
+            INDEX_ALBUM_NAME,
+            INDEX_DATE_ADDED
+        )
+        val selection =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) MediaStore.Images.Media.SIZE + " > 0"
+            else null
+        val sortOrder = "$INDEX_DATE_ADDED DESC"
+        val cursor = context.contentResolver.query(uri, projection, selection, null, sortOrder)
+
+        cursor?.let {
+            while(cursor.moveToNext()) {
+                val mediaPath = cursor.getString(cursor.getColumnIndex(INDEX_MEDIA_URI))
+                imageItemList.value!!.add(ImageItem(
+                    Uri.fromFile(File(mediaPath)), false
+                ))
+            }
+        }
+
+        cursor?.close()
+    }
+}
