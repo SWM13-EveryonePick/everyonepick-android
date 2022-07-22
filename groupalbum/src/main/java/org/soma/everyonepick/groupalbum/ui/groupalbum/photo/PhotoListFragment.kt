dc90eb8215fbdd20aef8c1fa7f960733f4d20aba
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-23 00:20:47 +0900

    [PICK-54] REFACTOR: PermissionFragment

--- groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/groupalbum/photo/PhotoListFragment.kt
+++ groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/groupalbum/photo/PhotoListFragment.kt
@@ -1,19 +1,26 @@
 package org.soma.everyonepick.groupalbum.ui.groupalbum.photo
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.MutableLiveData
 import dagger.hilt.android.AndroidEntryPoint
 import org.soma.everyonepick.groupalbum.adapter.PhotoAdapter
-import org.soma.everyonepick.groupalbum.data.PhotoDao
-import org.soma.everyonepick.groupalbum.data.PhotoItem
 import org.soma.everyonepick.groupalbum.databinding.FragmentPhotolistBinding
 import org.soma.everyonepick.groupalbum.utility.PhotoListMode
 import org.soma.everyonepick.groupalbum.viewmodel.GroupAlbumViewPagerViewModel
 import org.soma.everyonepick.groupalbum.viewmodel.PhotoListViewModel
+import java.io.File
+
 
 @AndroidEntryPoint
 class PhotoListFragment: Fragment() {
@@ -63,15 +70,42 @@ class PhotoListFragment: Fragment() {
     }
 
 
+    /*private val INDEX_MEDIA_ID = MediaStore.MediaColumns._ID
+    private val INDEX_MEDIA_URI = MediaStore.MediaColumns.DATA
+    private val INDEX_ALBUM_NAME = MediaStore.Images.Media.BUCKET_DISPLAY_NAME
+    private val INDEX_DATE_ADDED = MediaStore.MediaColumns.DATE_ADDED*/
+
     fun onClickUploadPhotoButton() {
-        // TODO: 사진 업로드로 대체
-        val id = viewModel.photoItemList.value!!.size.toLong()
-        viewModel.addPhotoItem(PhotoItem(
-            PhotoDao(id, "https://picsum.photos/200"),
-            false,
-            false
-        ))
+        // val PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
     }
+
+    /*@SuppressLint("Range")
+    private fun f() {
+        val uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        val projection = arrayOf(
+            INDEX_MEDIA_ID,
+            INDEX_MEDIA_URI,
+            INDEX_ALBUM_NAME,
+            INDEX_DATE_ADDED
+        )
+        val selection =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) MediaStore.Images.Media.SIZE + " > 0"
+            else null
+        val sortOrder = "$INDEX_DATE_ADDED DESC"
+        val cursor =
+            requireContext().contentResolver.query(uri, projection, selection, null, sortOrder)
+
+        val albumList = MutableLiveData<MutableList<Uri>>()
+        cursor?.let {
+            while(cursor.moveToNext()) {
+                val mediaPath = cursor.getString(cursor.getColumnIndex(INDEX_MEDIA_URI))
+                albumList.value?.add(Uri.fromFile(File(mediaPath)))
+                Log.e("ASD", Uri.fromFile(File(mediaPath)).toString())
+            }
+        }
+
+        cursor?.close()
+    }*/
 
     fun onClickDeleteButton() {
         viewModel.deleteCheckedItems()
