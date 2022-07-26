439e6b7e6b91edf3e1a04f22fdb3f69936fa68ba
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-26 16:58:24 +0900

    [PICK-57] FIX: CustomActionBar 전체가 뒤로가기 버튼 영역인 문제

--- common-ui/src/main/java/org/soma/everyonepick/common_ui/binding/ViewBinding.kt
+++ common-ui/src/main/java/org/soma/everyonepick/common_ui/binding/ViewBinding.kt
@@ -1,20 +1,20 @@
 package org.soma.everyonepick.common_ui.binding
 
 import android.content.ContextWrapper
+import android.view.View
 import android.widget.ImageView
 import androidx.activity.OnBackPressedDispatcherOwner
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import dagger.hilt.android.internal.managers.ViewComponentManager
-import org.soma.everyonepick.common_ui.CustomActionBar
 
 object ViewBinding {
     @JvmStatic
     @BindingAdapter("onBackPressed")
-    fun bindBackButton(view: CustomActionBar, onBackPressed: Boolean) {
+    fun bindBackButton(view: View, onBackPressed: Boolean) {
         var context = view.context
         // For Hilt
-        if(context is ViewComponentManager.FragmentContextWrapper){
+        if (context is ViewComponentManager.FragmentContextWrapper) {
             context = (context as ContextWrapper).baseContext
         }
         if (onBackPressed && context is OnBackPressedDispatcherOwner) {
