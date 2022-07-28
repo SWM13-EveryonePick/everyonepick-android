3e7af92fc4cc6134cd7434f2e6c7c4853907ef15
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-28 12:55:33 +0900

    [PICK-74] FEAT: keyword 검색 구현

--- groupalbum/src/main/java/org/soma/everyonepick/groupalbum/viewmodel/InviteFriendViewModel.kt
+++ groupalbum/src/main/java/org/soma/everyonepick/groupalbum/viewmodel/InviteFriendViewModel.kt
@@ -3,10 +3,16 @@ package org.soma.everyonepick.groupalbum.viewmodel
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.kakao.sdk.talk.TalkApiClient
 import com.kakao.sdk.talk.model.Friend
 import com.kakao.sdk.talk.model.Friends
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import org.soma.everyonepick.groupalbum.data.item.InviteFriendItem
 import org.soma.everyonepick.groupalbum.data.repository.FriendRepository
 import org.soma.everyonepick.groupalbum.data.repository.PhotoRepository
@@ -16,17 +22,24 @@ import javax.inject.Inject
 class InviteFriendViewModel @Inject constructor(
     private val friendRepository: FriendRepository
 ): ViewModel() {
+    val keyword = MutableStateFlow("")
     val inviteFriendItemList: MutableLiveData<MutableList<InviteFriendItem>> = MutableLiveData()
     val checked = MutableLiveData(0)
     val isApiLoading = MutableLiveData(true)
+
     init {
-        fetchInviteFriendItemList()
+        viewModelScope.launch {
+            keyword.collectLatest {
+                fetchInviteFriendItemList(it)
+            }
+        }
     }
 
-    fun fetchInviteFriendItemList() {
+    private fun fetchInviteFriendItemList(keyword: String) {
         isApiLoading.value = true
         friendRepository.fetchFriends({ isApiLoading.value = false }) { newFriends ->
             val newInviteFriendItemList = convertFriendsToInviteFriendItemList(newFriends)
+                .filter { it.friend.profileNickname?.contains(keyword) ?: false }.toMutableList()
             inviteFriendItemList.postValue(newInviteFriendItemList)
         }
     }
@@ -34,8 +47,8 @@ class InviteFriendViewModel @Inject constructor(
     private fun convertFriendsToInviteFriendItemList(friends: Friends<Friend>): MutableList<InviteFriendItem> {
         val result = mutableListOf<InviteFriendItem>()
         friends.elements?.let { friendList ->
-            for(friend in friendList) {
-                result.add(InviteFriendItem(friend, isChecked = false))
+            friendList.forEach {
+                result.add(InviteFriendItem(it, isChecked = false))
             }
         }
         return result
