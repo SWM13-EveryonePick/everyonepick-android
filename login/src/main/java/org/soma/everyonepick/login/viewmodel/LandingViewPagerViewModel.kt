7858e96d7f95b7131325a8c6b0301f9a444041e5
Hanbit-Kang <fchopinof99@gmail.com>
2022-07-30 12:45:58 +0900

    [PICK-75] FEAT: fragment_landing_view_pager으로 카카오 로그인 이동 및 디자인

--- login/src/main/java/org/soma/everyonepick/login/viewmodel/LandingViewPagerViewModel.kt
+++ login/src/main/java/org/soma/everyonepick/login/viewmodel/LandingViewPagerViewModel.kt
@@ -3,6 +3,7 @@ package org.soma.everyonepick.login.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class LoginViewModel: ViewModel() {
+class LandingViewPagerViewModel: ViewModel() {
+    val currentItem = MutableLiveData(0)
     val isApiLoading = MutableLiveData(false)
 }
