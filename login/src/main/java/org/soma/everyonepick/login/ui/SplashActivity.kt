f1800293cdcaf958ada6f94a2d85b77751e34ce9
Hanbit-Kang <fchopinof99@gmail.com>
2022-08-01 01:57:34 +0900

    [PICK-75] FEAT: Splash startTextViewAnimation()

--- login/src/main/java/org/soma/everyonepick/login/ui/SplashActivity.kt
+++ login/src/main/java/org/soma/everyonepick/login/ui/SplashActivity.kt
@@ -1,12 +1,15 @@
 package org.soma.everyonepick.login.ui
 
+import android.animation.ValueAnimator
 import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
 import org.soma.everyonepick.login.R
 import org.soma.everyonepick.login.databinding.ActivitySplashBinding
@@ -19,6 +22,7 @@ class SplashActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
         supportActionBar?.hide()
 
+        startTextViewAnimation()
         finishSplash()
     }
 
@@ -34,6 +38,22 @@ class SplashActivity : AppCompatActivity() {
         window?.let {
             it.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
             it.statusBarColor = Color.TRANSPARENT
+        }
+    }
+
+    private fun startTextViewAnimation() {
+        val descriptionParams = binding.textDescription.layoutParams as ConstraintLayout.LayoutParams
+        ValueAnimator.ofInt(100, 0).apply {
+            addUpdateListener { valueAnimator ->
+                binding.textTitle.alpha = animatedFraction
+                binding.textDescription.alpha = animatedFraction
+
+                descriptionParams.bottomMargin = valueAnimator.animatedValue as Int
+                binding.textDescription.layoutParams = descriptionParams
+            }
+            duration = 300
+
+            start()
         }
     }
 }
