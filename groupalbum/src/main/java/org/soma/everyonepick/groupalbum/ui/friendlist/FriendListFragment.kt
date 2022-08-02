4ba827fac68d2ee9199ff817c88160f93603b6ef
Hanbit-Kang <fchopinof99@gmail.com>
2022-08-02 16:26:59 +0900

    [PICK-80] FIX: FriendListFragment로 되돌아올 때 친구 리스트가 비어 보이는 문제

--- groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/friendlist/FriendListFragment.kt
+++ groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/friendlist/FriendListFragment.kt
@@ -1,6 +1,7 @@
 package org.soma.everyonepick.groupalbum.ui.friendlist
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -32,6 +33,11 @@ class FriendListFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.friends.value = viewModel.friends.value
     }
 
     override fun onDestroy() {
