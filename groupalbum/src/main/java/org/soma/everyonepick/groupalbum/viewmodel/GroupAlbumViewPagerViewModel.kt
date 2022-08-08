ea70d06b5f6537747b6d230625a09da9eca70069
Hanbit-Kang <fchopinof99@gmail.com>
2022-08-08 21:44:03 +0900

    [PICK-78] FEAT: 방장에게만 내보내기 버튼 활성화

--- groupalbum/src/main/java/org/soma/everyonepick/groupalbum/viewmodel/GroupAlbumViewPagerViewModel.kt
+++ groupalbum/src/main/java/org/soma/everyonepick/groupalbum/viewmodel/GroupAlbumViewPagerViewModel.kt
@@ -2,7 +2,12 @@ package org.soma.everyonepick.groupalbum.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import org.soma.everyonepick.common.data.model.User
+import org.soma.everyonepick.common.data.pref.PreferencesDataStore
 import org.soma.everyonepick.common.data.repository.UserRepository
 import org.soma.everyonepick.groupalbum.data.itemlist.MemberItemList
 import org.soma.everyonepick.groupalbum.data.model.GroupAlbum
@@ -11,18 +16,30 @@ import javax.inject.Inject
 
 @HiltViewModel
 class GroupAlbumViewPagerViewModel @Inject constructor(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val preferencesDataStore: PreferencesDataStore
 ): ViewModel() {
     // Fragment가 args를 통해 group album id를 가지고 있으므로, Fragment단에서 초기화를 진행합니다.
     val groupAlbum = MutableLiveData(GroupAlbum(-1, "Loading", -1,-1))
     val currentItem: MutableLiveData<Int> = MutableLiveData(0)
+    var me: User? = null
 
-    val photoSelectionMode = MutableLiveData(SelectionMode.NORMAL_MODE.ordinal)
-    // TODO: 합성중 / 합성완료 모드
-
+    // Drawer 관련
     val memberSelectionMode = MutableLiveData(SelectionMode.NORMAL_MODE.ordinal)
     var memberItemList = MutableLiveData(MemberItemList())
     val checked = MutableLiveData(0)
+
+    // 하위 Fragment들 관련
+    val photoSelectionMode = MutableLiveData(SelectionMode.NORMAL_MODE.ordinal)
+    // TODO: 합성중 / 합성완료 모드
+
+    init {
+        viewModelScope.launch {
+            me = preferencesDataStore.accessToken.first()?.let {
+                userRepository.getUser(it).data
+            }
+        }
+    }
 
     fun updateGroupAlbumTitle(newTitle: String) {
         val newGroupAlbum = groupAlbum.value?.copy(title = newTitle)
