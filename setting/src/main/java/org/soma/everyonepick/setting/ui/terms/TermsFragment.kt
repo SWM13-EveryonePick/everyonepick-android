9b2a004712cae7cec72c5c745f08f26ad096e8ae
Hanbit-Kang <fchopinof99@gmail.com>
2022-08-12 02:08:30 +0900

    [PICK-89] ADD: 이용약관 페이지 샘플 텍스트 추가

--- setting/src/main/java/org/soma/everyonepick/setting/ui/terms/TermsFragment.kt
+++ setting/src/main/java/org/soma/everyonepick/setting/ui/terms/TermsFragment.kt
@@ -5,14 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import org.soma.everyonepick.setting.R
+import org.soma.everyonepick.setting.databinding.FragmentTermsBinding
 
-class TermsFragment : Fragment() {
+class TermsFragment : Fragment(), TermsFragmentListener {
+    private var _binding: FragmentTermsBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_terms, container, false)
+        _binding = FragmentTermsBinding.inflate(inflater, container, false).also {
+            it.lifecycleOwner = this
+            it.listener = this
+        }
+        return binding.root
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+
+    /** TermsFragmentListener */
+    override fun onClickTermsOfServiceButton() {
+        val directions = TermsFragmentDirections.toTermsOfServiceFragment()
+        findNavController().navigate(directions)
+    }
+
+    override fun onClickPrivacyPolicyButton() {
+        val directions = TermsFragmentDirections.toPrivacyPolicyFragment()
+        findNavController().navigate(directions)
+    }
+}
+
+interface TermsFragmentListener {
+    fun onClickTermsOfServiceButton()
+    fun onClickPrivacyPolicyButton()
 }
