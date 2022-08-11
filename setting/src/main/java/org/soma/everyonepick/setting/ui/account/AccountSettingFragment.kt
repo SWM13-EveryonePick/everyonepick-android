d05e309ad63ad826a05e4d8e2c486359eb6184f2
Hanbit-Kang <fchopinof99@gmail.com>
2022-08-11 17:00:42 +0900

    [PICK-89] ADD: 로그아웃, 회원탈퇴 버튼

--- setting/src/main/java/org/soma/everyonepick/setting/ui/account/AccountSettingFragment.kt
+++ setting/src/main/java/org/soma/everyonepick/setting/ui/account/AccountSettingFragment.kt
@@ -6,13 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import org.soma.everyonepick.setting.R
+import org.soma.everyonepick.setting.databinding.FragmentAccountSettingBinding
 
 class AccountSettingFragment : Fragment() {
+    private var _binding: FragmentAccountSettingBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account_setting, container, false)
+        _binding = FragmentAccountSettingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
