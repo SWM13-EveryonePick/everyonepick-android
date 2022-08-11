4784718adb5a14a1e190dbbe04a646bfde22ad9b
Hanbit-Kang <fchopinof99@gmail.com>
2022-08-11 15:46:47 +0900

    [PICK-89] ADD: 설정 내부 뎁스 프래그먼트

---
+++ setting/src/main/java/org/soma/everyonepick/setting/ui/account/AccountSettingFragment.kt
@@ -0,0 +1,18 @@
+package org.soma.everyonepick.setting.ui.account
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.soma.everyonepick.setting.R
+
+class AccountSettingFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account_setting, container, false)
+    }
+}
