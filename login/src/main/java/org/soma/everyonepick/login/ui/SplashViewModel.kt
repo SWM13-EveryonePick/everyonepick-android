f40798ef1ca0451035df4e0397bd8d5bafa5a7fc
Hanbit-Kang <fchopinof99@gmail.com>
2022-08-19 22:54:42 +0900

    [PICK-101] REFACTOR: SplashViewModel StateFlow 사용 및 외부에서 데이터 수정 직접 불가

--- login/src/main/java/org/soma/everyonepick/login/ui/SplashViewModel.kt
+++ login/src/main/java/org/soma/everyonepick/login/ui/SplashViewModel.kt
@@ -2,11 +2,24 @@ package org.soma.everyonepick.login.ui
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 /**
  * [SplashActivity]에서의 비동기 작업의 성공 및 실패 카운트 정보를 홀드합니다.
  */
 class SplashViewModel: ViewModel() {
-    val success = MutableLiveData(0)
-    val failure = MutableLiveData(0)
+    private val _success = MutableStateFlow(0)
+    var success: StateFlow<Int> = _success
+
+    private val _failure = MutableStateFlow(0)
+    var failure: StateFlow<Int> = _failure
+
+    fun addSuccess() {
+        _success.value += 1
+    }
+
+    fun addFailure() {
+        _failure.value += 1
+    }
 }
