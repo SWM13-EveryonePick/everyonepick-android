f3fcb439443fd88277fc65a24ff768a5a24de3d8
Hanbit-Kang <fchopinof99@gmail.com>
2022-08-20 16:10:25 +0900

    [PICK-101] REFACTOR: FriendListViewModel에 StateFlow 적용

--- groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/friendlist/FriendListViewModel.kt
+++ groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/friendlist/FriendListViewModel.kt
@@ -4,6 +4,9 @@ import androidx.lifecycle.ViewModel
 import com.kakao.sdk.talk.model.Friend
 import com.kakao.sdk.talk.model.Friends
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import org.soma.everyonepick.common.util.KakaoUtil
 import org.soma.everyonepick.groupalbum.domain.usecase.FriendUseCase
 import javax.inject.Inject
 
@@ -11,13 +14,16 @@ import javax.inject.Inject
 class FriendListViewModel @Inject constructor(
     private val friendUseCase: FriendUseCase
 ): ViewModel() {
-    val friends: MutableLiveData<Friends<Friend>> = MutableLiveData()
-    val isApiLoading = MutableLiveData(true)
+    private val _friends: MutableStateFlow<Friends<Friend>> = MutableStateFlow(KakaoUtil.emptyFriends)
+    val friends: StateFlow<Friends<Friend>> = _friends
+
+    private val _isApiLoading = MutableLiveData(true)
+    val isApiLoading = _isApiLoading
 
     fun readFriends() {
         isApiLoading.value = true
         friendUseCase.readFriends({ isApiLoading.value = false }) { newFriends ->
-            friends.postValue(newFriends)
+            _friends.value = newFriends
         }
     }
 }
