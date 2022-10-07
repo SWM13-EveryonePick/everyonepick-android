62371c591202b28dda58ed961a3b64d0bc9e9a49
Hanbit-Kang <fchopinof99@gmail.com>
2022-10-07 15:39:47 +0900

    [PICK-96] ADD: fragment_pick_info 디자인

--- groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/groupalbumlist/groupalbum/pickinfo/PickInfoFragment.kt
+++ groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/groupalbumlist/groupalbum/pickinfo/PickInfoFragment.kt
@@ -6,13 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 import org.soma.everyonepick.groupalbum.R
 import org.soma.everyonepick.groupalbum.databinding.FragmentPickInfoBinding
 
+@AndroidEntryPoint
 class PickInfoFragment : Fragment() {
 
     private var _binding: FragmentPickInfoBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: PickInfoViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
---
+++ groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/groupalbumlist/groupalbum/pickinfo/PickInfoViewModel.kt
@@ -0,0 +1,7 @@
+package org.soma.everyonepick.groupalbum.ui.groupalbumlist.groupalbum.pickinfo
+
+import androidx.lifecycle.ViewModel
+
+class PickInfoViewModel: ViewModel() {
+
+}
