260a867577d0ad9b4afdb6066e889c0af6bcb1a8
Hanbit-Kang <fchopinof99@gmail.com>
2022-10-07 16:01:22 +0900

    [PICK-96] FEAT: fragment_pick_info에 userCount, pickUserCount databinding

--- groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/groupalbumlist/groupalbum/pickinfo/PickInfoViewModel.kt
+++ groupalbum/src/main/java/org/soma/everyonepick/groupalbum/ui/groupalbumlist/groupalbum/pickinfo/PickInfoViewModel.kt
@@ -1,7 +1,30 @@
 package org.soma.everyonepick.groupalbum.ui.groupalbumlist.groupalbum.pickinfo
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import org.soma.everyonepick.groupalbum.domain.model.PickInfoModel
+import javax.inject.Inject
 
-class PickInfoViewModel: ViewModel() {
+@HiltViewModel
+class PickInfoViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle
+): ViewModel() {
+    val userCount = savedStateHandle[USER_COUNT]?: 0
+    val pickUserCount = savedStateHandle[PICK_USER_COUNT]?: 0
 
+    private val _timeout = MutableStateFlow(savedStateHandle[TIMEOUT]?: 0L)
+    val timeout: StateFlow<Long> = _timeout
+
+    init {
+        // TODO: timeout 감소
+    }
+
+    companion object {
+        private const val USER_COUNT = "userCount"
+        private const val PICK_USER_COUNT = "pickUserCount"
+        private const val TIMEOUT = "timeout"
+    }
 }
