6fa30aeef6f160c11702a1c1ec90102590876232
Hanbit-Kang <fchopinof99@gmail.com>
2022-10-14 11:35:17 +0900

    [PICK-150] ADD: delete result photo list 관련 DTO, service 함수

---
+++ groupalbum/src/main/java/org/soma/everyonepick/groupalbum/data/dto/ResultPhotoIdListRequest.kt
@@ -0,0 +1,8 @@
+package org.soma.everyonepick.groupalbum.data.dto
+
+import org.soma.everyonepick.groupalbum.data.entity.PhotoId
+
+// ResultPhotoRequestDto
+data class ResultPhotoIdListRequest(
+    val resultPhotos: List<PhotoId>
+)
--- groupalbum/src/main/java/org/soma/everyonepick/groupalbum/data/source/remote/GroupAlbumResultPhotoService.kt
+++ groupalbum/src/main/java/org/soma/everyonepick/groupalbum/data/source/remote/GroupAlbumResultPhotoService.kt
@@ -1,14 +1,21 @@
 package org.soma.everyonepick.groupalbum.data.source.remote
 
+import org.soma.everyonepick.groupalbum.data.dto.ResultPhotoIdListRequest
 import org.soma.everyonepick.groupalbum.data.dto.ResultPhotoListResponse
-import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface GroupAlbumResultPhotoService {
     @GET("api/album/{groupAlbumId}/result")
     suspend fun readResultPhotoList(
         @Header("Authorization") token: String,
         @Path("groupAlbumId") groupAlbumId: Long
+    ): ResultPhotoListResponse
+
+    // @DELETE에선 Body가 없는 것이 원칙이기 때문에 오류가 발생하였고, 아래 코드를 통해 허용할 수 있었습니다.
+    @HTTP(method="DELETE", path="api/album/{groupAlbumId}/result", hasBody = true)
+    suspend fun deleteResultPhotoList(
+        @Header("Authorization") token: String,
+        @Path("groupAlbumId") groupAlbumId: Long,
+        @Body resultPhotos: ResultPhotoIdListRequest
     ): ResultPhotoListResponse
 }
