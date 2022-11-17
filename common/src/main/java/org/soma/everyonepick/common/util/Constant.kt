3c57472100fb6099388da7d17cba9d4232b047e6
Hanbit-Kang <fchopinof99@gmail.com>
2022-11-17 15:06:36 +0900

    [PICK-161] MODIFY: 이용약관 URL 변경

--- common/src/main/java/org/soma/everyonepick/common/util/Constant.kt
+++ common/src/main/java/org/soma/everyonepick/common/util/Constant.kt
@@ -6,8 +6,8 @@ const val DATABASE_NAME = "everyonepick-db"
 
 const val CONTACT_EMAIL_ADDRESS = "everyonepickofficial@gmail.com"
 
-const val TERMS_OF_SERVICE_URL = "https://everyonepick-bucket.s3.ap-northeast-2.amazonaws.com/static/모두의%2BPICK%2B이용약관.html"
-const val PRIVACY_POLICY_URL = "https://everyonepick-bucket.s3.ap-northeast-2.amazonaws.com/static/모두의%2BPICK%2B개인정보%2B처리방침.html"
+const val TERMS_OF_SERVICE_URL = "https://dosf6do8h1hli.cloudfront.net/static/%E1%84%86%E1%85%A9%E1%84%83%E1%85%AE%E1%84%8B%E1%85%B4%2BPICK%2B%E1%84%8B%E1%85%B5%E1%84%8B%E1%85%AD%E1%86%BC%E1%84%8B%E1%85%A3%E1%86%A8%E1%84%80%E1%85%AA%E1%86%AB.html"
+const val PRIVACY_POLICY_URL = "https://dosf6do8h1hli.cloudfront.net/static/%E1%84%86%E1%85%A9%E1%84%83%E1%85%AE%E1%84%8B%E1%85%B4%2BPICK%2B%E1%84%80%E1%85%A2%E1%84%8B%E1%85%B5%E1%86%AB%E1%84%8C%E1%85%A5%E1%86%BC%E1%84%87%E1%85%A9%2B%E1%84%8E%E1%85%A5%E1%84%85%E1%85%B5%E1%84%87%E1%85%A1%E1%86%BC%E1%84%8E%E1%85%B5%E1%86%B7.html"
 
 const val SHOWING_UP_ANIMATION_Y_OFFSET = 50
 const val SHOWING_UP_ANIMATION_DURATION = 500L
