7bbb4d01efad23b39da5f1452efb122e1d755e14
Hanbit-Kang <fchopinof99@gmail.com>
2022-11-17 12:50:56 +0900

    Update BASE_URL

--- foundation/src/main/java/org/soma/everyonepick/foundation/util/Constant.kt
+++ foundation/src/main/java/org/soma/everyonepick/foundation/util/Constant.kt
@@ -1,4 +1,4 @@
 package org.soma.everyonepick.foundation.util
 
-const val BASE_URL = "http://www.everyonepick.com:8080/"
+const val BASE_URL = "https://www.everyonepick.com/"
 const val LOCAL_BASE_URL = "http://10.0.2.2:8080/"
