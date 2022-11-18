e53f3fc6dd3bdd7f471a3c4b853bcc7f1125e8d6
Hanbit-Kang <fchopinof99@gmail.com>
2022-11-18 18:35:51 +0900

    [PICK-167] FEAT: onNewToken에 Hilt 적용

--- app/src/main/java/org/soma/everyonepick/app/fcm/MyFirebaseMessagingService.kt
+++ app/src/main/java/org/soma/everyonepick/app/fcm/MyFirebaseMessagingService.kt
@@ -1,7 +1,18 @@
 package org.soma.everyonepick.app.fcm
 
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import dagger.hilt.android.AndroidEntryPoint
+import org.soma.everyonepick.common.domain.usecase.UserUseCase
+import javax.inject.Inject
 
-class MyFirebaseMessagingService : FirebaseMessagingService() {
+@AndroidEntryPoint
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+    @Inject lateinit var userUseCase: UserUseCase
 
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d("FcmToken", token)
+        // TODO: userUseCase.updateFcmToken(token)
+    }
 }
